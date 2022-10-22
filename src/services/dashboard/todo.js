2f4adfce740b75bc40b7b902d29562bb2d73e9d8
pien <pincenna@gmail.com>
2022-10-22 13:36:06 +0700

    menambahkan fungsi edit untuk service todo

--- src/services/dashboard/todo.js
+++ src/services/dashboard/todo.js
@@ -5,6 +5,6 @@ const api = '/api/todo';
 const list = () => baseApi.get(`${api}`);
 const add = (body) => baseApi.post(`${api}`, body);
 const del = () => baseApi.get(`${api}`);
-const edit = () => baseApi.get(`${api}`);
+const edit = (id, body) => baseApi.get(`${api}/${id}`, body);
 
 export { list, add, del, edit };
